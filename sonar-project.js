b6ecddc51b65537f248ceaaaff0173a0bab9797d
mimialive <111397470+mimialive@users.noreply.github.com>
2022-09-28 16:22:17 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://54.211.155.166/:9000/',
+  serverUrl: 'http://http://54.211.155.166:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
